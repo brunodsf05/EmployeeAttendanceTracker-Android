949326ac4a86665b135303f5e107fed713fe7edc
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-01-29 11:37:37 +0100

    Gui: Diseñado lo básico de la pantalla de fichajes

--- app/src/main/java/bdisfer1410/controldepresencia/MainActivity.java
+++ app/src/main/java/bdisfer1410/controldepresencia/MainActivity.java
@@ -1,15 +1,19 @@
 package bdisfer1410.controldepresencia;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +24,28 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        Button buttonFichar = findViewById(R.id.btnClock);
+
+        // Configurar la toolbar
+        toolbar.setTitle(R.string.bar_title);
+        setSupportActionBar(toolbar);
+    }
+
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Añade las opciones a la Toolbar
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        // Muestra los íconos
+        if (menu instanceof MenuBuilder) {
+            MenuBuilder menuBuilder = (MenuBuilder) menu;
+            menuBuilder.setOptionalIconsVisible(true);
+        }
+
+        return true;
     }
 }
