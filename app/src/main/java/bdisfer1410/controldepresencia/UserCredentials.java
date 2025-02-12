8e1389e1ef9c3eeb029c176ee13c99bc5a81f967
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-02-12 12:30:38 +0100

    Data: Clase para almacenar credenciales de inicio de sesÃión

---
+++ app/src/main/java/bdisfer1410/controldepresencia/UserCredentials.java
@@ -0,0 +1,35 @@
+package bdisfer1410.controldepresencia;
+
+public class UserCredentials {
+    private String name;
+    private String password;
+
+    public UserCredentials(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isNameValid() {
+        return !name.isBlank();
+    }
+
+    public boolean isPasswordValid() {
+        return !password.isBlank();
+    }
+}
