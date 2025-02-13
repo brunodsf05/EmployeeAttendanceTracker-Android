1f4407d961b44164bf276f264a061d97572e860e
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-02-13 21:44:21 +0100

    Feat: Clase que recoge la respuesta de Authentication

---
+++ app/src/main/java/bdisfer1410/controldepresencia/login/AuthResponse.java
@@ -0,0 +1,19 @@
+package bdisfer1410.controldepresencia.login;
+
+class AuthResponse {
+    private boolean success;
+    private String token;
+    private String message;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
