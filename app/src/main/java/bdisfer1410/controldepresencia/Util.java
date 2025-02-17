0f887c0faf300ee14d36c65f1bbdeae04c7f8018
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-02-17 21:31:42 +0100

    Feat: Util.getMessage maneja mejor el caso de que no consiga mensaje.

--- app/src/main/java/bdisfer1410/controldepresencia/Util.java
+++ app/src/main/java/bdisfer1410/controldepresencia/Util.java
@@ -5,7 +5,38 @@ import android.content.Context;
 
 public class Util {
     /**
-     * Obtiene el texto
+     * Obtiene un mensaje de ``strings.xml`` según la clave.
+     *
+     * @param context Contexto de la aplicación.
+     * @param key La clave del recurso de texto que se desea obtener.
+     * @param fallbackMessage El mensaje a mostrar cuando la clave no exista.
+     *
+     * @return El mensaje que guarda {@code key}. Si no lo encuentra devuelve {@code fallbackMessage}.
+     */
+    public static String getMessage(Context context, String key, String fallbackMessage) {
+        @SuppressLint("DiscouragedApi")
+        int resId = context.getResources().getIdentifier(key, "string", context.getPackageName());
+
+        return (resId != 0)
+                ? context.getString(resId)
+                : key;
+    }
+
+    /**
+     * Obtiene un mensaje de ``strings.xml`` según la clave.
+     *
+     * @param context Contexto de la aplicación.
+     * @param key La clave del recurso de texto que se desea obtener.
+     * @param fallbackResId El código del mensaje a mostrar cuando la clave no exista.
+     *
+     * @return El mensaje que guarda {@code key}. Si no lo encuentra devuelve el mensaje de {@code fallbackResId}.
+     */
+    public static String getMessage(Context context, String key, int fallbackResId) {
+        return getMessage(context, key, context.getString(fallbackResId));
+    }
+
+    /**
+     * Obtiene un mensaje de ``strings.xml`` según la clave.
      *
      * @param context Contexto de la aplicación.
      * @param key La clave del recurso de texto que se desea obtener.
@@ -13,11 +44,6 @@ public class Util {
      * @return El mensaje que guarda {@code key}. Si no lo encuentra devuelve el texto de {@code key}.
      */
     public static String getMessage(Context context, String key) {
-        @SuppressLint("DiscouragedApi")
-        int resId = context.getResources().getIdentifier(key, "string", context.getPackageName());
-
-        return (resId != 0)
-                ? context.getString(resId)
-                : key;
+        return getMessage(context, key, key);
     }
 }
