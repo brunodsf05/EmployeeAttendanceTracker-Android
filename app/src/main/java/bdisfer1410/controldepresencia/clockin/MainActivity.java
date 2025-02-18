4ffcd3883d8e394a73c2aa0d128015b4677ede4e
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-02-18 12:49:51 +0100

    Docs: Separado en secciones MainActivity.java

--- app/src/main/java/bdisfer1410/controldepresencia/clockin/MainActivity.java
+++ app/src/main/java/bdisfer1410/controldepresencia/clockin/MainActivity.java
@@ -20,6 +20,7 @@ import bdisfer1410.controldepresencia.R;
 
 
 public class MainActivity extends AppCompatActivity {
+    // Android
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
