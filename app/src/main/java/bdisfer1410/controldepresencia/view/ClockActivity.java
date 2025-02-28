ca14336459fc3f39f59e8d84bc427579e996d257
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-02-28 22:51:32 +0100

    Feat: La app consigue la localización

--- app/src/main/java/bdisfer1410/controldepresencia/view/ClockActivity.java
+++ app/src/main/java/bdisfer1410/controldepresencia/view/ClockActivity.java
@@ -1,8 +1,14 @@
 package bdisfer1410.controldepresencia.view;
 
 
+import static android.view.View.VISIBLE;
+import static android.view.View.GONE;
+
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -16,9 +22,13 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.view.menu.MenuBuilder;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 import bdisfer1410.controldepresencia.R;
 import bdisfer1410.controldepresencia.api.ApiClient;
@@ -32,19 +42,26 @@ import bdisfer1410.controldepresencia.models.Tokens;
 
 public class ClockActivity extends AppCompatActivity {
     //region Variables
+    //region Configuración
+    private static final int PERMISSION_LOCATION_REQUEST_CODE = 1;
+    private static final String KEY_LATESTCLOCKSTATE = "LATEST_CLOCK_STATE";
+    //endregion
+
     //region Views
     private Toolbar toolbar;
     private Button buttonClock;
-    private TextView feedbackTitle, feedbackDescription;
+    private TextView feedbackTitle, feedbackDescription, feedbackError, feedbackWarning;
     //endregion
 
     //region Estado
     private ApiService service;
+    private FusedLocationProviderClient fusedLocationClient;
     //endregion
 
     //region Datos
     private Tokens tokens;
     private ClockAction latestClockAction;
+    private Location latestLocation;
     //endregion
     //endregion
 
@@ -63,6 +80,10 @@ public class ClockActivity extends AppCompatActivity {
         findViews();
         configureViews();
 
+        // Manejar la geolocalización
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        updateLocation();
+
         // Cargar el token
         Intent intent = getIntent();
 
@@ -74,6 +95,19 @@ public class ClockActivity extends AppCompatActivity {
             tokens = new Tokens(intent);
             Log.d("TOKEN", String.format("El Intent recibio el de %s", tokens.access.getDebug()));
             Log.d("TOKEN", String.format("El Intent recibio el de %s", tokens.refresh.getDebug()));
+        }
+
+        // Restaurar pantalla tras rotar
+        if (savedInstanceState != null) {
+            latestClockAction = (ClockAction) savedInstanceState.getSerializable(KEY_LATESTCLOCKSTATE);
+            if (latestClockAction != null) {
+                Log.d("RESTORATION", String.format("Se restauró latestClockAction a: %s", latestClockAction.name()));
+                configureClockInterface();
+                return;
+            }
+            else {
+                Log.w("RESTORATION", "latestClockAction era null en savedInstanceState.");
+            }
         }
 
         // Verificar acción de fichaje
@@ -120,6 +154,38 @@ public class ClockActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == PERMISSION_LOCATION_REQUEST_CODE) {
+            if (grantResults.length > 0) {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.d("LOCATION", "¡Se consiguió el permiso de ubicación precisa!");
+                    updateLocation();
+                }
+                else {
+                    Log.e("LOCATION", "Se denegó el permiso de ubicación precisa.");
+                    setFeedbackError(getString(R.string.clock_error_location_preciserequired));
+                }
+            }
+            else {
+                Log.e("LOCATION", "No se recibieron resultados del permiso.");
+            }
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putSerializable(KEY_LATESTCLOCKSTATE, latestClockAction);
+    }
+
+    private void setFeedbackError(String message) {
+        feedbackError.setText(message);
+        feedbackError.setVisibility((message.isEmpty()) ? GONE : VISIBLE);
+    }
+
     //region Configuración
     //region Inicial
     private void findViews() {
@@ -127,11 +193,15 @@ public class ClockActivity extends AppCompatActivity {
         buttonClock = findViewById(R.id.btnClock);
         feedbackTitle = findViewById(R.id.feedbackTitle);
         feedbackDescription = findViewById(R.id.feedbackDescription);
+        feedbackError = findViewById(R.id.feedbackError);
+        feedbackWarning = findViewById(R.id.feedbackWarning);
     }
 
     private void configureViews() {
         toolbar.setTitle(R.string.clock_title);
         setSupportActionBar(toolbar);
+
+        setFeedbackError("");
 
         buttonClock.setOnClickListener(v -> {
             Log.d("CLICK", "Fichar");
@@ -143,12 +213,53 @@ public class ClockActivity extends AppCompatActivity {
     private void configureClockInterface() {
         Log.d("GUI", String.format("Configurando la interfaz con: %s", latestClockAction.name()));
 
+        feedbackWarning.setVisibility(GONE);
         buttonClock.setEnabled(latestClockAction.canClock());
         buttonClock.setText(latestClockAction.getButtonText());
         feedbackTitle.setText(latestClockAction.getFeedbackTitle());
         feedbackDescription.setText(latestClockAction.getFeedbackDescription());
+
+        boolean canApplyNoLocationStyiling = latestLocation == null && latestClockAction.doesLocationMatter();
+
+        if (canApplyNoLocationStyiling) {
+            buttonClock.setEnabled(ClockAction.TOBEIN_WORK.canClock());
+            feedbackWarning.setVisibility(VISIBLE);
+        }
     }
     //endregion
+    //endregion
+
+    //region Geolocalización
+    private void updateLocation() {
+        latestLocation = null;
+
+        boolean doesntHasLocationPermissions = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED;
+
+        if (doesntHasLocationPermissions) {
+            Log.e("LOCATION", "No se tienen permisos de ubicación.");
+            Log.d("LOCATION", "Preguntando permisos de ubicación...");
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_LOCATION_REQUEST_CODE);
+            configureClockInterface();
+            return;
+        }
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, location -> {
+                    if (location == null) {
+                        setFeedbackError(getString(R.string.clock_error_location_couldntfetch));
+                        Log.e("LOCATION", "No se pudo leer la ubicación");
+                    }
+                    else {
+                        Log.d("LOCATION", "¡Se consiguió la ubicación!");
+                        Log.d("LOCATION", String.format("Latitud/Longitud %f/%f", location.getLatitude(), location.getLongitude() ));
+                        setFeedbackError("");
+                        latestLocation = location;
+                    }
+
+                    configureClockInterface();
+                });
+    }
+
     //endregion
 
     //region Menú de tres puntos
