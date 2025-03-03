3666a9777eda8cc005b85337985fc593f1499c12
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-03-03 15:32:27 +0100

    Feat: Se ordeno la lógica sobre las horas

--- app/src/main/java/bdisfer1410/controldepresencia/tools/Hour.java
+++ app/src/main/java/bdisfer1410/controldepresencia/tools/Hour.java
@@ -4,20 +4,20 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class Hour {
-    public static final String FORMAT = "H:mm";
     public static final String INVALID = "...";
 
-    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern(FORMAT);
+    public static final DateTimeFormatter HOUR_FORMATTER = DateTimeFormatter.ofPattern("HH:mm:ss");
+    public static final DateTimeFormatter STRING_FORMATTER = DateTimeFormatter.ofPattern("H:mm");
 
     public static String format(LocalTime time) {
         return (time == null)
                 ? INVALID
-                : time.format(FORMATTER);
+                : time.format(STRING_FORMATTER);
     }
 
     public static LocalTime parse(String time) {
         return (time == null)
                 ? null
-                : LocalTime.parse(time, FORMATTER);
+                : LocalTime.parse(time, HOUR_FORMATTER);
     }
 }
