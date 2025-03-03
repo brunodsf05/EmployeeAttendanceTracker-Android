5e61fea517becb1f2c275978b1cb125be5f5082d
brunodsf05 <brunodsf05.profesional@gmail.com>
2025-03-03 14:47:04 +0100

    Feat: ClockResponse recibe las horas

--- app/src/main/java/bdisfer1410/controldepresencia/api/clock/ClockResponse.java
+++ app/src/main/java/bdisfer1410/controldepresencia/api/clock/ClockResponse.java
@@ -1,19 +1,51 @@
 package bdisfer1410.controldepresencia.api.clock;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import bdisfer1410.controldepresencia.models.ClockAction;
 
 public class ClockResponse {
     private final String action;
 
-    public ClockResponse(String action) {
+    @SerializedName("hora_entrada")
+    private String startHourString;
+
+    @SerializedName("hora_salida")
+    private String exitHourString;
+
+    public ClockResponse(String action, String startHourString, String exitHourString) {
         this.action = action;
+        this.startHourString = startHourString;
+        this.exitHourString = exitHourString;
     }
 
     public String getActionString() {
         return action;
     }
 
+    public String getStartHourString() {
+        return startHourString;
+    }
+
+    public String getExitHourString() {
+        return exitHourString;
+    }
+
     public ClockAction getAction() {
         return ClockAction.fromString(action);
+    }
+
+    private LocalDateTime parseHour(String hour) {
+        return LocalDateTime.parse(hour, DateTimeFormatter.ISO_DATE_TIME);
+    }
+    public LocalDateTime getStartHour() {
+        return parseHour(startHourString);
+    }
+
+    public LocalDateTime getExitHour() {
+        return parseHour(exitHourString);
     }
 }
