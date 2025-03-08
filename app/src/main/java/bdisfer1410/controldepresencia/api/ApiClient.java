e916462b9950e181b61f8ea68db1190122d42c73
brunodsf05 <231746160+brunodsf05@users.noreply.github.com>
2025-03-08 18:29:19 +0100

    Fix: El cliente enviará Dates en formato ISO 8601

--- app/src/main/java/bdisfer1410/controldepresencia/api/ApiClient.java
+++ app/src/main/java/bdisfer1410/controldepresencia/api/ApiClient.java
@@ -1,14 +1,21 @@
 package bdisfer1410.controldepresencia.api;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
     private static final String BASE_URL = "https://bdisfer1410.eu.pythonanywhere.com/";
 
+    private static final Gson myGson = new GsonBuilder()
+            .setDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'") // Formato ISO 8601
+            .create();
+
     public static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(myGson))
             .build();
 }
 
